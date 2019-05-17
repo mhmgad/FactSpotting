aa38cdfb508ba2240c9f66fe7c66d343652b8296
gadelrab <gadelrab@mpi-inf.mpg.de>
2019-05-17 10:00:14 -0700

    url for entities

---
+++ src/main/java/de/mpii/factspotting/text/verbalization/dictionarybased/ElasticDictionary.java
@@ -0,0 +1,26 @@
+package de.mpii.factspotting.text.verbalization.dictionarybased;
+
+import de.mpii.factspotting.text.verbalization.TextParaphrase;
+
+import java.util.Collection;
+
+public class ElasticDictionary implements IDictionary<TextParaphrase> {
+
+
+
+
+
+    @Override
+    public Collection<TextParaphrase> getParaphrases(String key) {
+        return null;
+    }
+
+    @Override
+    public Collection<TextParaphrase> getParaphrases(String key, int topk) {
+        return null;
+    }
+
+
+
+
+}
